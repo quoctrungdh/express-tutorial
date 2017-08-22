c03676a382e1ea585eb95f52d47aafde3b0e3fbd
Do Ha Quoc Trung <quoc-trung.do-ha@ekino.com>
2017-08-22 14:04:14 +0700

    feat: add middleware for protecting authentication require APIs

--- server/index.js
+++ server/index.js
@@ -3,14 +3,23 @@ const app = express();
 
 // routers for /users
 const users = require('./routers/users.js');
-app.use('/users', users);
-
 // routers for /user/:id details
 const user = require('./routers/user.js');
-app.use('/user', user);
 
 app.get('/', function(req, res) {
-   res.send('home')
+    res.send('home')
+ })
+
+// middleware to protect authenticated APIs
+app.use(function(req, res, next) {
+    if(true) {
+        next();
+    }
+    res.send('Authentication is required!')
 })
+
+// place authen APIs behind middleware to be protect
+app.use('/users', users);
+app.use('/user', user);
 
 app.listen(3001)
